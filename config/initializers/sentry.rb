b8c9c57e7d01f6eddd8db157eb1169b83fe4e2ca
Derik Olsson <do@derik.co>
2025-10-29 15:34:16 -0500

    Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,20 @@
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.dig(:sentry_dsn)
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 0.0
+
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 0.0
+
+  # Add data like request headers and IP for users
+  config.send_default_pii = true
+
+  # Only enable in production
+  config.enabled_environments = %w[production]
+end
