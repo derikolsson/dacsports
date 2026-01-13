b75b7dfa30ad2fd6e4ef8ff53506b943151dd4fb
Derik Olsson <do@derik.co>
2026-01-13 00:11:27 -0500

    Team logos, mascots, and the like

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def team_badge(team)
+    content_tag(:span, team.abbreviation,
+      class: "team-badge",
+      style: "background-color: #{team.primary_color}; color: #fff;",
+      title: team.name)
+  end
+
+  def team_chip(team)
+    link_to team_path(team.slug),
+      class: "team-chip",
+      style: "background-color: color-mix(in srgb, #{team.primary_color} 12%, white); color: #{team.text_color};" do
+      team_badge(team) + content_tag(:span, team.name, class: "team-chip-name")
+    end
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -9,6 +9,16 @@ class Team < ApplicationRecord
     "Richland"
   ].freeze
 
+  TEAM_COLORS = {
+    "brookhaven"    => { primary: "#1B5E20", secondary: "#000000", abbr: "BH", mascot: "Bears" },
+    "cedar-valley"  => { primary: "#E65100", secondary: "#000000", abbr: "CV", text: "#BF360C", mascot: "Suns" },
+    "eastfield"     => { primary: "#1565C0", secondary: "#EF6C00", abbr: "EF", mascot: "Harvester Bees" },
+    "el-centro"     => { primary: "#4A148C", secondary: "#00BCD4", abbr: "EC", mascot: "Eagles" },
+    "mountain-view" => { primary: "#1565C0", secondary: "#FFC107", abbr: "MV", mascot: "Lions" },
+    "north-lake"    => { primary: "#2E7D32", secondary: "#1976D2", abbr: "NL", mascot: "Blazers" },
+    "richland"      => { primary: "#4A148C", secondary: "#FFC107", abbr: "RC", mascot: "Thunderducks" }
+  }.freeze
+
   has_many :event_teams, dependent: :destroy
   has_many :events, through: :event_teams
 
@@ -32,5 +42,29 @@ class Team < ApplicationRecord
 
   def mini_logo_exists?
     File.exist?(Rails.root.join("public", "team-logos", "mini", "#{slug}.png"))
+  end
+
+  def team_colors
+    TEAM_COLORS[slug] || { primary: "#6c757d", secondary: "#ffffff", abbr: name[0..2].upcase }
+  end
+
+  def primary_color
+    team_colors[:primary]
+  end
+
+  def secondary_color
+    team_colors[:secondary]
+  end
+
+  def abbreviation
+    team_colors[:abbr]
+  end
+
+  def text_color
+    team_colors[:text] || primary_color
+  end
+
+  def mascot
+    team_colors[:mascot]
   end
 end
